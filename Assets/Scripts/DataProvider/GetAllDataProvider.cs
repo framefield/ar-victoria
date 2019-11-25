52c8629d97c47b5c5b6d22bba34afc03a7a4119b
Tobias <patrick.tobias.fischer@fu-berlin.de>
2019-11-25 15:28:46 +0100

    Initial commit.

---
+++ Assets/Scripts/DataProvider/GetAllDataProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetAllDataProvider : IDataProvider {
+
+    public override object GetData()
+    {
+        return annotations;
+    }
+}
---
+++ Assets/Scripts/Project.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Project  {
+    public Dictionary<string,string> ids = new Dictionary<string, string>();
+    public string name;
+    public IDataService provider;
+}

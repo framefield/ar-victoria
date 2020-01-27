2a1d7be85aae868f978ed9722bf099ce96e93863
Dominik <dominik.ganghofer@framefield.com>
2020-01-27 18:04:50 +0100

    Create ff and fu folders

--- Assets/Scripts/Project.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Project  {
-    public Dictionary<string,string> ids = new Dictionary<string, string>();
-    public string name;
-    public IDataService provider;
-}

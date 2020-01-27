b559a53ab1b4a4b4bd02f6810a87e8ac35eb859a
Tobias <patrick.tobias.fischer@fu-berlin.de>
2020-01-27 09:16:45 +0100

    Prototyping Dwelltimer added to Cursor. Also checking in some files that were not under version control yet.

---
+++ Assets/Scripts/Gesture/NodShakeRecognizer2.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class NodShakeRecognizer2 : MonoBehaviour {
+
+    private Vector3[] angles;
+    private int index;
+    private Vector3 centerAngle;
+    private int sampleLength = 40;
+
+    public event Action NodHandler;
+    public event Action HeadshakeHandler;
+    public static NodShakeRecognizer2 Current { get; private set; }
+
+    void Awake()
+    {
+        Current = this;
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        angles = new Vector3[sampleLength];
+        index = 0;
+        centerAngle = Camera.main.transform.rotation.eulerAngles;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        angles[index] = Camera.main.transform.rotation.eulerAngles;
+        //Debug.Log("Index: " + index + " Angle: " + centerAngle.x);
+        index++;
+        
+        if (index == sampleLength)
+        {
+            CheckMovement();
+            ResetGesture();
+        }
+
+    }
+
+    void CheckMovement()
+    {
+        int up = 0, down = 0;
+        for(int i = 0; i < sampleLength; i++)
+        {
+            if(angles[i].x < centerAngle.x - 3f && down < 2)
+            {
+                down++;
+            }else if(angles[i].x > centerAngle.x  && up < 1)
+            {
+                up++;
+            }
+        }
+
+        if(up == 1 && down ==2)
+        {
+            Debug.Log("Yes");
+            NodHandler?.Invoke();
+        }
+
+    }
+
+    void ResetGesture()
+    {
+        angles = new Vector3[sampleLength];
+        index = 0;
+        centerAngle = Camera.main.transform.rotation.eulerAngles;
+
+    }
+}
---
+++ Assets/Scripts/Gesture/nodShakeHandler3.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nodShakeHandler3 : MonoBehaviour
+{
+
+    public AudioSource yesSound;
+    public AudioSource noSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Register Callback
+        NodShakeRecognizer3.Current.HeadshakeHandler += OnHadShake;
+        NodShakeRecognizer3.Current.NodHandler += OnNod;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnNod()
+    {
+        noSound.Play();
+    }
+
+    void OnHadShake()
+    {
+        yesSound.Play();
+    }
+}

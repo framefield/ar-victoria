5f1a9af6ff66430fc81be7b4ce799c0b17a45871
dominik <gdominik@posteo.de>
2020-02-03 15:11:25 +0100

    Add StatueInteraction

---
+++ Assets/ff/StatueInteraction/StatueInteraction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using victoria;
+using victoria.bodyParts;
+
+public class StatueInteraction : MonoBehaviour
+{
+    [SerializeField] private BodyPartData<MeshRenderer> _meshes;
+    [SerializeField] private BodyPartRenderers _renderers;
+    
+    public interface IInteractionListener
+    {
+        void OnHover();
+        void OnUnhover();
+    }
+}

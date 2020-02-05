3dcc01d6d6859a35b59d6a284c44b356712233fd
dominik <gdominik@posteo.de>
2020-02-05 17:06:17 +0100

    Add camera controls via mouse input for development

--- Assets/ff/DesktopDebugController.cs
+++ Assets/ff/DesktopDebugController.cs
@@ -7,8 +7,29 @@ namespace victoria
         [SerializeField] private float _speed = .05f;
         [SerializeField] private float _rotationSpeed = 1f;
 
+        private Vector3 _lastMousePosition;
         private void Update()
         {
+            if (Input.GetMouseButtonDown(0))
+                _lastMousePosition = Input.mousePosition;
+
+            if (Input.GetMouseButton(0))
+            {
+                var delta = Input.mousePosition - _lastMousePosition;
+                
+                var camRotation = transform.eulerAngles;
+                var x = camRotation.x - delta.y * _rotationSpeed;
+                if (x > 180f)
+                    x -= 360f;            
+                        transform.rotation =
+                    Quaternion.Euler(
+                        Mathf.Clamp(x, -90f, 90f),
+                        camRotation.y + delta.x * _rotationSpeed,
+                        camRotation.z);
+                
+                AddRotation(delta*_rotationSpeed);
+            }
+            
             if (Input.GetKey(KeyCode.A))
                 transform.position += _speed * transform.TransformVector(Vector3.left);
             if (Input.GetKey(KeyCode.S))

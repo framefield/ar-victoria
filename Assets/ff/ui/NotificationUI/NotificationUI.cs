b4dc8ebb3eb7d5fb818d3e6d604f578b4b3f3800
dominik <gdominik@posteo.de>
2020-02-15 14:17:43 +0100

    Add NotificationUI

---
+++ Assets/ff/ui/NotificationUI/NotificationUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NotificationUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _label;
+    [SerializeField] private bool _showDebugNotifications;
+
+    public void ShowNotifiation(string text, float durationInSeconds = 2f)
+    {
+        _timeLeftToShowNotification = durationInSeconds;
+        _label.text = text;
+    }
+
+    public void ShowDebugNotification(string text, float durationInSeconds = 2f)
+    {
+        if (_showDebugNotifications)
+            ShowNotifiation(text, durationInSeconds);
+    }
+
+    private void Update()
+    {
+        if (_timeLeftToShowNotification < 0)
+            return;
+
+        _timeLeftToShowNotification -= Time.deltaTime;
+        _label.gameObject.SetActive(_timeLeftToShowNotification >= 0);
+    }
+    
+    private float _timeLeftToShowNotification;
+}
